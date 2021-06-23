82a13370ac9619cc871ed1e83fb15a2e1ff2375b
vagrant <hbhandal80@hotmail.com>
2021-06-23 04:25:19 +0000

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@revenue = Store.sum(:annual_revenue)
+@average = Store.average(:annual_revenue)
+@overmil = Store.where("annual_revenue > 1000000").count
+
+puts "\nThe annual revenue for all stores is #{@revenue}"
+
+puts "\nThe average annual revenue for all stores is #{@average}"
+
+puts "\nThere are #{@overmil} stores generating over $1M or more in annual sales"
